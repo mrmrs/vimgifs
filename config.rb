3e37ad05d38544f979a9d4de140c3525ada1c08d
Sam Mason <samm@lostmy.name>
2016-07-23 10:20:05 +0100

    Add tachyons to project rather than pulling from cdn

--- config.rb
+++ config.rb
@@ -9,11 +9,12 @@ page '/*.json', layout: false
 
 # General configuration
 config[:layout] = 'default'
+config[:css_dir] = 'css'
 
 activate :blog do |blog|
   blog.sources = 'gifs/:title.html'
   blog.permalink = '{title}.html'
-  blog.layout = 'gifs'
+  blog.layout = 'gif'
   blog.default_extension = '.erb'
   blog.new_article_template = "#{Dir.pwd}/new_gif_template.erb"
 end
